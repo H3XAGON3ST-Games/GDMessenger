f21a410bc48b92ded053c2920fedaef72399f369
Nikita Vtorushin <vtorushin-nikitos@mail.ru>
2023-05-25 20:17:07 +0500

    remove unnecessary requests from a gmessenger.sql

--- gmessenger.sql
+++ gmessenger.sql
@@ -38,36 +38,6 @@ ALTER TABLE "chat_has_message" ADD FOREIGN KEY ("id_user") REFERENCES "user_clie
 
 ALTER TABLE "chat_has_message" ADD FOREIGN KEY ("id_chat") REFERENCES "chat" ("id");
 
-INSERT INTO "user_client"
-VALUES
-('h3xa', 'h3xa'), 
-('test', 'test'),
-('esti', 'esti'),
-('4321', '4321');
-
-INSERT INTO chat
-VALUES
-('h3xa_test', null), 
-('h3xa_esti', null), 
-('test_esti', null);
-
-INSERT INTO user_has_chat
-VALUES
-('h3xa', 'h3xa_test'),
-('test', 'h3xa_test'),
-('h3xa', 'h3xa_esti'), 
-('esti', 'h3xa_esti'),
-('test', 'test_esti'),  
-('esti', 'test_esti');
-
-INSERT INTO chat_has_message
-VALUES
-('h3xa', 'h3xa_test', 'да'),
-('h3xa', 'h3xa_esti', 'тоже да'), 
-('test', 'h3xa_test', 'че да'), 
-('esti', 'h3xa_esti', 'да че да-то?'), 
-('esti', 'test_esti', 'понял');
-
 CREATE OR REPLACE FUNCTION get_user_data(IN nickname varchar)
 RETURNS SETOF user_client AS $$
 BEGIN
@@ -101,10 +71,3 @@ BEGIN
   RETURN;
 END;
 $$ LANGUAGE plpgsql;
-
-SELECT user_has_chat.id_user, tchat.id_chat 
-FROM user_has_chat 
-JOIN (SELECT id_chat FROM get_user_chat_list('h3xa')) tchat on user_has_chat.id_chat = tchat.id_chat 
-WHERE user_has_chat.id_user != 'h3xa'
-
-SELECT id_user, "message_text", id_message FROM chat_has_message WHERE id_chat = 1
